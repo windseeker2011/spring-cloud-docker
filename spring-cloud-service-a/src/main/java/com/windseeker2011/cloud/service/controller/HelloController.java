f488e781b9b893a9a87c9f448441acb7babf8c17
Weihai Li <windseeker2011@163.com>
2019-03-15 15:46:16 +0800

    单机多实例部署采用随机端口即可。

--- spring-cloud-service-a/src/main/java/com/windseeker2011/cloud/service/controller/HelloController.java
+++ spring-cloud-service-a/src/main/java/com/windseeker2011/cloud/service/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.windseeker2011.cloud.service.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,8 +26,9 @@ public class HelloController {
 	}
 
 	@GetMapping(value = "/haha")
-	public String haha() {
-		return "haha";
+	public String haha(HttpServletRequest request) {
+		
+		return "haha : " + request.getServerPort();
 	}
 
 }
